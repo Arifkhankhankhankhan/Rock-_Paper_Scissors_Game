c22123baa5cdf6a475372b53ab9f64d99d417ebd
Arifkhankhankhankhan-Git <arifkhan93246057@gmail.com>
2024-06-10 01:48:39 -0700

    react commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [yourScore, setYourScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [outputMessage, setOutputMessage] = useState('');
+  const choices = ['✊', '✋', '✌'];
+
+  const computerChoice = () => {
+      const randomChoice = Math.floor(Math.random() * 3);
+      return choices[randomChoice];
+  }
+
+  const handleButtonClick = (choice) => {
+      const computerChoiceValue = computerChoice();
+      let result = '';
+
+      if (choice === computerChoiceValue) {
+          result = "It's Tie!!!";
+      } else if ((choice === '✊' && computerChoiceValue === '✌') ||
+                 (choice === '✋' && computerChoiceValue === '✊') ||
+                 (choice === '✌' && computerChoiceValue === '✋')) {
+          result = 'You Win!!!';
+          setYourScore(prevScore => prevScore + 1);
+      } else {
+          result = 'You Lose!!!';
+          setComputerScore(prevScore => prevScore + 1);
+      }
+
+      setOutputMessage(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <div className="header">
+              <h1>Rock Paper Scissor</h1>
+          </div>
+          <div className="score">
+              <p>Your Score: <span>{yourScore}</span></p>
+              <p>Computer Score: <span>{computerScore}</span></p>
+          </div>
+          <div className="game">
+              {choices.map(choice => (
+                  <button key={choice} onClick={() => handleButtonClick(choice)}>
+                      {choice}
+                  </button>
+              ))}
+          </div>
+          <div className="output">
+              {outputMessage && <p>{outputMessage}</p>}
+          </div>
+      </div>
   );
 }
 
